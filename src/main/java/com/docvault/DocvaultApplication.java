b213054005ce0b0bdca26c2a83c43ff8861c8cab
Arjun Avadhani <arjunavadhani22@gmail.com>
2026-02-23 20:44:59 -0500

    feat(Story 1.1): Add Initial Spring Boot scaffold

---
+++ src/main/java/com/docvault/DocvaultApplication.java
@@ -0,0 +1,13 @@
+package com.docvault.docvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocvaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocvaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/docvault/DocvaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docvault.docvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocvaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
